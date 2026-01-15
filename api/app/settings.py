2d7ada6c4b291305be7bc9ddb483fee16afe1113
Nathan Christopher Menon <menonnathanchristopher@gmail.com>
2026-01-15 16:35:51 +0530

    summarization bottleneck resolved

--- api/app/settings.py
+++ api/app/settings.py
@@ -2,7 +2,7 @@ from pydantic import BaseModel
 import os
 
 class Settings(BaseModel):
-    pg_host : str = os.getenv("PGHOST","host.docker.internal")
+    pg_host : str = os.getenv("PGHOST", "localhost")
     pg_db : str = os.getenv("PGDATABASE", "postgres")
     pg_user : str = os.getenv("PGUSER", "postgres")
     pg_password : str = os.getenv("PGPASSWORD", "postgres")
